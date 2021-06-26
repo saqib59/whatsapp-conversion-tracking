a40b59eea507d18ca1d31bd11de9f2ca627b6245
saqib59 <saqibali80400@gmail.com>
2021-06-26 13:25:05 +0500

    whatsapp renamed to wa

--- wa-conversion-tracking.php
+++ wa-conversion-tracking.php
@@ -9,12 +9,12 @@ License: GPLv2
 Author URI: https://www.fiverr.com/saqibali862
 Requires at least: 5.0
 Tested up to: 5.7
-Text Domain: word-replacer-ultra
+Text Domain: whatsapp-tracker
  */
 
 define('WHATSAPP_CONV_TRACK_PATH', dirname(__FILE__));
 $plugin = plugin_basename(__FILE__);
 define('WHATSAPP_CONV_TRACK_URL', plugin_dir_url($plugin));
 
-require WHATSAPP_CONV_TRACK_PATH . '/inc/whatsapp-tracker-main.php';
-require WHATSAPP_CONV_TRACK_PATH . '/inc/whatsapp-tracker-ajax.php';
+require WHATSAPP_CONV_TRACK_PATH . '/inc/wa-tracker-main.php';
+require WHATSAPP_CONV_TRACK_PATH . '/inc/wa-tracker-ajax.php';
